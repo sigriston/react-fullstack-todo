873956735f50c1e9bb1ee79ada86f6424aaeca97
Thiago Sigrist <sigrist@gmail.com>
2018-01-15 00:19:35 -0200

    todosActionTypes

--- client/src/actions/todos.js
+++ client/src/actions/todos.js
@@ -1,6 +1,7 @@
 import * as api from '../api/todos';
+import { GET_TODOS } from '../constants/todosActionTypes';
 
 export const getTodos = () => ({
-  type: 'GET_TODOS',
+  type: GET_TODOS,
   payload: api.getTodos()
 });
--- client/src/reducers/todos.js
+++ client/src/reducers/todos.js
@@ -1,6 +1,9 @@
+import { GET_TODOS } from '../constants/todosActionTypes';
+import { FULFILLED } from 'redux-promise-middleware';
+
 const todosReducer = (state = [], action) => {
   switch (action.type) {
-    case 'GET_TODOS_FULFILLED':
+    case `${GET_TODOS}_${FULFILLED}`:
       return action.payload;
     default:
       return state;

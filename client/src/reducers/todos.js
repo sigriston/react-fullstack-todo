94b923765f8f979a68cdf701ad3c13c21bbe8690
Thiago Sigrist <sigrist@gmail.com>
2018-01-15 00:24:17 -0200

    implement todosReducer with typeToReducer

--- client/src/reducers/todos.js
+++ client/src/reducers/todos.js
@@ -1,13 +1,11 @@
+import typeToReducer from 'type-to-reducer';
 import { GET_TODOS } from '../constants/todosActionTypes';
 import { FULFILLED } from 'redux-promise-middleware';
 
-const todosReducer = (state = [], action) => {
-  switch (action.type) {
-    case `${GET_TODOS}_${FULFILLED}`:
-      return action.payload;
-    default:
-      return state;
+const todosReducer = typeToReducer({
+  [GET_TODOS]: {
+    [FULFILLED]: (_, action) => action.payload
   }
-};
+}, []);
 
 export default todosReducer;
